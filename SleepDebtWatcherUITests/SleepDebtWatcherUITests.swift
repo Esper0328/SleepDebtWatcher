af8d54946c5a2ef49b260ec31db73cad6c1afe1b
esper0328 <esper0328@gmail.com>
2018-02-25 14:10:47 +0900

    changed parameter of bedtimeInputTest shorter

--- SleepDebtWatcherUITests/SleepDebtWatcherUITests.swift
+++ SleepDebtWatcherUITests/SleepDebtWatcherUITests.swift
@@ -100,10 +100,10 @@ class SleepDebtWatcherUITests: XCTestCase {
         bedtimePlanTest(sleep_hour : "9", sleep_minute : "55", sleep_AMorPM : "PM",wake_hour : "4", wake_minute : "55", wake_AMorPM : "AM")
         
         bedtimeInputTest(sleep_date : "Jan 17", sleep_hour : "9", sleep_minute : "55", sleep_AMorPM : "PM",
-                         wake_date : "Jan 18", wake_hour : "4", wake_minute : "55", wake_AMorPM : "AM", sleepDebtValue: "1.0")
+                         wake_date : "Jan 18", wake_hour : "2", wake_minute : "55", wake_AMorPM : "AM", sleepDebtValue: "3.0")
         bedtimeInputTest(sleep_date : "Jan 18", sleep_hour : "9", sleep_minute : "55", sleep_AMorPM : "PM",
-                         wake_date : "Jan 19", wake_hour : "4", wake_minute : "55", wake_AMorPM : "AM", sleepDebtValue: "2.0")
-        bedtimeInputTest(sleep_date : "Jan 19", sleep_hour : "9", sleep_minute : "55", sleep_AMorPM : "PM",
+                         wake_date : "Jan 19", wake_hour : "1", wake_minute : "55", wake_AMorPM : "AM", sleepDebtValue: "7.0")
+/*        bedtimeInputTest(sleep_date : "Jan 19", sleep_hour : "9", sleep_minute : "55", sleep_AMorPM : "PM",
                          wake_date : "Jan 20", wake_hour : "4", wake_minute : "55", wake_AMorPM : "AM", sleepDebtValue: "3.0")
         bedtimeInputTest(sleep_date : "Jan 20", sleep_hour : "9", sleep_minute : "55", sleep_AMorPM : "PM",
                          wake_date : "Jan 21", wake_hour : "4", wake_minute : "55", wake_AMorPM : "AM", sleepDebtValue: "4.0")
@@ -112,7 +112,7 @@ class SleepDebtWatcherUITests: XCTestCase {
         bedtimeInputTest(sleep_date : "Jan 22", sleep_hour : "9", sleep_minute : "55", sleep_AMorPM : "PM",
                          wake_date : "Jan 23", wake_hour : "4", wake_minute : "55", wake_AMorPM : "AM", sleepDebtValue: "6.0")
         bedtimeInputTest(sleep_date : "Jan 23", sleep_hour : "9", sleep_minute : "55", sleep_AMorPM : "PM",
-                         wake_date : "Jan 24", wake_hour : "4", wake_minute : "55", wake_AMorPM : "AM", sleepDebtValue: "7.0")
+                         wake_date : "Jan 24", wake_hour : "4", wake_minute : "55", wake_AMorPM : "AM", sleepDebtValue: "7.0")*/
     }
     
     func goAndBackEachViewFromTopViewTest() {
